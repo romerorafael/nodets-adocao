65a3c483777fd9230090d724511016428e09cab6
rafaromeropolen <rafa.romero@opolen.com.br>
2022-02-21 23:50:10 -0300

    Primeiro Commit com os dados inclusos

--- src/controllers/page.controller.ts
+++ src/controllers/page.controller.ts
@@ -1,17 +1,47 @@
 import { Request, Response} from 'express';
+import { createMenuObjects } from '../helpers/createMenuObject';
+import { Pet } from '../models/Pet.model';
 
 export const home = (req: Request, res: Response)=>{
-    // res.render('pages/page');
+    res.render('pages/page', {
+        menu: createMenuObjects('all'),
+        banner:{
+            title: "Todos os animais",
+            background: "allanimals.jpg"
+        }, 
+        pets: Pet.getAll()
+    });
 }
 
 export const dogs = (req: Request, res: Response)=>{
-    // res.render('pages/page');
+    res.render('pages/page', {
+        menu: createMenuObjects('dog'),
+        banner:{
+            title: "Cachorros",
+            background: "banner_dog.jpg"
+        },
+        pets: Pet.getByType('dog')
+    });
 }
 
 export const cats = (req: Request, res: Response)=>{
-    // res.render('pages/page');
+    res.render('pages/page', {
+        menu: createMenuObjects('cat'),
+        banner:{
+            title: "Gatos",
+            background: "banner_cat.jpg"
+        },
+        pets: Pet.getByType('cat')
+    });
 }
 
 export const fishes = (req: Request, res: Response)=>{
-    // res.render('pages/page');
+    res.render('pages/page', {
+        menu: createMenuObjects('fish'),
+        banner:{
+            title: "Peixes",
+            background: "banner_fish.jpg"
+        },
+        pets: Pet.getByType('fish')
+    });
 }
---
+++ src/models/Pet.model.ts
@@ -0,0 +1,163 @@
+type PetType = 'dog' | 'cat' | 'fish';
+
+type Pet = {
+    name: string,
+    type: PetType,
+    image: string,
+    race: string, 
+    color: string, 
+    gender: 'Masculino' | 'Feminino'
+}
+
+const data: Pet[] = [
+    {
+        name: 'GoddBoy',
+        type: 'dog',
+        image: 'pastor-alemao.jpg',
+        race: 'Pastor-alemão',
+        color: 'Amarelo e Preto',
+        gender: 'Masculino'
+    },
+    {
+        name: 'GoddBoy',
+        type: 'dog',
+        image: 'labrador.jpg',
+        race: 'Labrador-retriever',
+        color: 'Branco',
+        gender: 'Masculino'
+    },
+    {
+        name: 'GoddBoy',
+        type: 'dog',
+        image: 'zwergspitz.jpg',
+        race: 'Zwergspitz',
+        color: 'Amarelo',
+        gender: 'Feminino'
+    },
+    {
+        name: 'GoddBoy',
+        type: 'dog',
+        image: 'husky.jpg',
+        race: 'Husky Siberiano',
+        color: 'Branco e Preto',
+        gender: 'Masculino'
+    },
+    {
+        name: 'GoddBoy',
+        type: 'dog',
+        image: 'golden.jpg',
+        race: 'Golden Retriever',
+        color: 'Amarelo',
+        gender: 'Masculino'
+    },
+    {
+        name: 'GoddBoy',
+        type: 'dog',
+        image: 'poodle.jpg',
+        race: 'Poodle',
+        color: 'Branco',
+        gender: 'Feminino'
+    },
+    {
+        name: 'GoddBoy',
+        type: 'dog',
+        image: 'bulldog.jpg',
+        race: 'Bulldog',
+        color: 'Branco e Amarelo',
+        gender: 'Masculino'
+    },
+    {
+        name: 'GoddBoy',
+        type: 'cat',
+        image: 'persa.jpg',
+        race: 'Persa',
+        color: 'Amarelo',
+        gender: 'Masculino'
+    },
+    {
+        name: 'GoddBoy',
+        type: 'cat',
+        image: 'mainecoon.jpg',
+        race: 'Maine Coon',
+        color: 'Preto e Branco',
+        gender: 'Masculino'
+    },
+    {
+        name: 'GoddBoy',
+        type: 'cat',
+        image: 'bengal.jpg',
+        race: 'Bengal',
+        color: 'Branco, Preto e Amarelo',
+        gender: 'Feminino'
+    },
+    {
+        name: 'GoddBoy',
+        type: 'cat',
+        image: 'siames.jpg',
+        race: 'Siamês',
+        color: 'Amarelo e Preto',
+        gender: 'Masculino'
+    },
+    {
+        name: 'GoddBoy',
+        type: 'cat',
+        image: 'sphynx.jpg',
+        race: 'Sphynx',
+        color: 'Branco',
+        gender: 'Masculino'
+    },
+    {
+        name: 'GoddBoy',
+        type: 'fish',
+        image: 'neon.jpg',
+        race: 'Tetra Neon',
+        color: 'Vermelho e Azul',
+        gender: 'Masculino'
+    },
+    {
+        name: 'GoddBoy',
+        type: 'fish',
+        image: 'matogrosso.jpg',
+        race: 'Mato Grosso',
+        color: 'Laranja',
+        gender: 'Masculino'
+    },
+    {
+        name: 'GoddBoy',
+        type: 'fish',
+        image: 'limpavidro.jpg',
+        race: 'Limpa Vidro',
+        color: 'Verde e Branco',
+        gender: 'Masculino'
+    },
+    {
+        name: 'GoddBoy',
+        type: 'fish',
+        image: 'tanictis.jpg',
+        race: 'Tanictis',
+        color: 'Vermelho',
+        gender: 'Masculino'
+    },
+    {
+        name: 'GoddBoy',
+        type: 'fish',
+        image: 'acara.jpg',
+        race: 'Acará Bandeira',
+        color: 'Preto',
+        gender: 'Masculino'
+    }
+]
+
+export const Pet = {
+    getAll: (): Pet[] => {
+        return data;
+    },
+    getByType: (type: PetType): Pet[] => {
+        return data.filter(pet => pet.type === type);
+    },
+    getByRace: (race: string): Pet[] =>{
+        return data.filter( pet => 
+            pet.race.toLocaleLowerCase().indexOf(race.toLocaleLowerCase()) > -1
+        )
+    }
+}
